9ffd796692ac3878543f4358105f0d8d6dc90896
ers <ersleeer@163.com>
2023-02-22 11:13:22 +0800

    new_wp & free_wp

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -6,9 +6,11 @@
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
 
-void init_wp_pool() {
+void init_wp_pool()
+{
   int i;
-  for (i = 0; i < NR_WP; i ++) {
+  for (i = 0; i < NR_WP; i++)
+  {
     wp_pool[i].NO = i;
     wp_pool[i].next = &wp_pool[i + 1];
   }
@@ -20,4 +22,23 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP *new_wp()
+{
+  if (free_->next == NULL)
+  {
+    printf("No free wp, new_wp fail!\n");
+    assert(0);
+  }
 
+  WP *new_wp = free_;
+  free_ = free_->next;
+  new_wp->next = head;
+  head = new_wp;
+  return new_wp;
+}
+
+void free_wp(WP *wp)
+{
+  wp->next = free_;
+  free_ = wp;
+}
