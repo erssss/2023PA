8cfa56a5d1b0c936673f648464dd56c8d974fa32
tracer-ics2017 <tracer@njuics.org>
2023-03-16 06:15:30 -0700

    >  compile 161220000 ers Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  06:15:30 up 17:05,  1 user,  load average: 0.27, 0.38, 0.22 c167483337db9fec3d73ee4638cb2071c50f45a0

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -191,8 +191,8 @@ uint32_t hex_to_dec(char str[32]) {
     uint result = 0;
     for (int i = 2; i < 10; ++i) {
         int tmp = 0;
-        if (str[i] >= '0' && str[i] <= '9') {
           printf("\t %c",str[i]);
+        if (str[i] >= '0' && str[i] <= '9') {
             tmp = (int)str[i] - (int)'0';
         } else if (str[i] >= 'a' && str[i] <= 'f') {
             tmp = (int)str[i] - (int)'a' + 10;
