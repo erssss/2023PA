b8e7ef30d5f3726a13dc810470ee4d88960dd98b
ers <ersleeer@163.com>
2023-04-10 20:37:29 +0800

    test

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,19 +25,29 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
+  // TODO();
+  decoding.is_jmp=1;
+
+  rtl_li(&t2,decoding.seq_eip);
+  rtl_push(&t2);
 
   print_asm("call %x", decoding.jmp_eip);
 }
 
 make_EHelper(ret) {
-  TODO();
-
+  // TODO();
+  rtl_pop(&t2);
+  decoding.jmp_eip = t2;
+  decoding.is_jmp = 1;
   print_asm("ret");
 }
 
 make_EHelper(call_rm) {
-  TODO();
+  // TODO();
+  rtl_li(&t2,decoding.seq_eip);
+  rtl_push(&t2);
+  decoding.jmp_eip=id_dest->val;
+  decoding.is_jmp=1;
 
   print_asm("call *%s", id_dest->str);
 }
