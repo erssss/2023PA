4420c79020486158f95b280e44110106b2da1888
tracer-ics2017 <tracer@njuics.org>
2023-04-15 18:41:48 -0700

    >  compile 161220000 ers Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:41:47 up 11:31,  1 user,  load average: 0.15, 0.03, 0.01 aecd672d6987e81a2effb679a9b6402a42dbdc

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -33,4 +33,10 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
 
 void _draw_sync() {}
 
-int _read_key() { return _KEY_NONE; }
+int _read_key () {
+  if (inb (0x64)) // 状态寄存器
+    return inl(0x60) ; // 数据寄存器
+  else
+    return _KEY_NONE;
+}
+
