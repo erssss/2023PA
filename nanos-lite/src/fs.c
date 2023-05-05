7d0181a8a3286e003af75299b92c996a786e181c
tracer-ics2017 <tracer@njuics.org>
2023-05-04 19:31:15 -0700

    >  run 161220000 ers Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:31:15 up 1 day,  7:04,  1 user,  load average: 0.63, 0.73, 0.57 d1e66915372d1e45befcf442951b4a3a5786c220

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,9 +1,12 @@
 #include "fs.h"
 
+extern void ramdisk_read(void *buf,off_t offset,size_t len);
+extern void ramdisk_write(void *buf,off_t offset,size_t len);
 typedef struct {
-  char *name;
-  size_t size;
-  off_t disk_offset;
+  char *name;         // 文件名
+  size_t size;        // 文件大小
+  off_t disk_offset;  // 文件在ramdisk上的偏移
+  off_t open_offset;  // 文件被打开之后的读写指针
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -24,3 +27,39 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+#define concat(x, y) x ## y
+#define GET_FS_POINTER(ptr) \
+size_t concat(fs,ptr)(int fd){ \
+  assert(fd>=0&&fd<NR_FILES); \
+  return file_table[fd].ptr;}
+
+
+GET_FS_POINTER(size);
+// //返回文件大小
+// size_t fs_filesz(int fd){
+//   assert(fd>=0&&fd<NR_FILES);
+//   return file_table[fd].size;
+// }
+
+// //磁盘偏移
+// off_t disk_offset(int fd){
+//   assert(fd>=0&&fd<NR_FILES);
+//   return file_table[fd].disk_offset;
+// }
+
+// //读写指针
+// off_t get_open_offset(int fd){
+//   assert(fd>=0&&fd<NR_FILES);
+//   return file_table[fd].open_offset;
+// }
+
+// //将读写偏移指针设置为n
+// void set_open_offset(int fd,int n){
+//   assert(fd>=0&&fd<NR_FILES);
+//   assert(n>=0);
+//   if(n>file_table[fd].size){
+//     n=file_table[fd].size;
+//   }
+//   file_table[fd].open_offset=n;
+// }
