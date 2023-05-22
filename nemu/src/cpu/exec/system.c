4cada4d882af25f224730b0780da555b47707b0b
tracer-ics2017 <tracer@njuics.org>
2023-05-21 19:24:08 -0700

    >  compile 161220000 ers Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  19:24:08 up 2 days, 18:36,  1 user,  load average: 0.12, 0.03, 0.01 b990b1a99b484fd961d7e133dd0b11ff38b247ba

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -24,12 +24,14 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
+  Log("mov_r2cr todo!");
   TODO();
 
   print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
+  Log("mov_cr2r todo!");
   TODO();
 
   print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
