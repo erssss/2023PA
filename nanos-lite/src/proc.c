de38e75a68352173c94851c5ac35a4bad8764975
tracer-ics2017 <tracer@njuics.org>
2023-05-26 09:53:50 -0700

    >  run 161220000 ers Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  09:53:50 up 3 days,  5:49,  1 user,  load average: 0.50, 0.54, 0.32 fae670cbfcfc3ffed500757223e76130a612b727

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -39,7 +39,7 @@ void load_prog(const char *filename) {
 
 #define FREQUENCY 1000
 _RegSet *schedule(_RegSet *prev) {
-  printf("schedule");
+    // printf("schedule");
     if (current != NULL)    // 如果当前进程的PCB指针不为空
         current->tf = prev; // 保存tf
     else
