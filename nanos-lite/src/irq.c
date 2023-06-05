ed524dc557b5ad8477cda60dd8b5f963a3df440e
tracer-ics2017 <tracer@njuics.org>
2023-06-04 18:38:00 -0700

    >  run 161220000 ers Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  18:38:00 up 3 days, 18:36,  1 user,  load average: 0.27, 0.17, 0.08 e4f90da3bfea58f6b2f9dfc590d67d095cb67194

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,14 +5,14 @@ extern _RegSet *do_syscall(_RegSet *r);
 static _RegSet *do_event(_Event e, _RegSet *r) {
     switch (e.event) {
     case (_EVENT_SYSCALL):
-        printf("syscall!\n");
+        // printf("syscall!\n");
         do_syscall(r);
         return schedule(r);
     case (_EVENT_TRAP):
-        printf("Self-trapped!\n");
+        // printf("Self-trapped!\n");
         return schedule(r);
     case _EVENT_IRQ_TIME:
-        printf("IRQ_TIME!\n");
+        // printf("IRQ_TIME!\n");
         return schedule(r);
     default:
         panic("Unhandled event ID = %d", e.event);
